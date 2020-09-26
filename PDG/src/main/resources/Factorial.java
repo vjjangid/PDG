9f26a042f668d3cf32f304a3f1832674ee3ab685
vjjangid <vjjangid850@gmail.com>
2020-09-26 21:25:57 +0530

    updated factorial resource file

--- PDG/src/main/resources/Factorial.java
+++ PDG/src/main/resources/Factorial.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Factorial {
 
+	int fact(int n)
+	{
+		if(n<=1)
+			return 1;
+		else
+			return n*fact(n-1);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		int ans = fact(n);
+		System.out.println(ans);
 
 	}
 
