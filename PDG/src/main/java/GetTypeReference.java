16c39389b4489e834f1ac6db3170ef4cfae33f2c
vjjangid <vjjangid850@gmail.com>
2020-09-30 16:34:21 +0530

    Experimenting with type solver

---
+++ PDG/src/main/java/GetTypeReference.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+
+public class GetTypeReference {
+
+	private static final String FILE_PATH = "src/main/resources/Bar.java";
+	
+	public static void main(String[] args) throws FileNotFoundException{
+
+		
+			TypeSolver typeSolver = new CombinedTypeSolver();
+			
+			JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+			StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+			CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
+			
+			cu.findAll(AssignExpr.class).forEach( ae -> {
+				ResolvedType resolvedType = ae.calculateResolvedType();
+				System.out.println(ae.toString() + " is  a :: " + resolvedType);
+			});
+			
+		
+		
+
+	}
+
+}
